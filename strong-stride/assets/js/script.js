3cbd22703aea06beceaa3c6ce0e2318795ff09a9
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-03-11 23:42:37 +0800

    finished responsiveness

--- strong-stride/assets/js/script.js
+++ strong-stride/assets/js/script.js
@@ -1,13 +1,19 @@
 "use strict";
 
+// SELECTORS
 const header = document.querySelector(".header");
+const navigationList = document.querySelector(".navigation__list");
 const btnMobileNav = document.getElementById("btn-mobile-open");
 const iconMobile = document.querySelector(".icon-mobile");
 
-btnMobileNav.addEventListener("click", () => {
+const showNavigation = () => {
+  // Toggle active class
   header.classList.toggle("active");
 
+  // Switch icon
   iconMobile.getAttribute("name") === "menu"
     ? iconMobile.setAttribute("name", "close")
     : iconMobile.setAttribute("name", "menu");
-});
+};
+
+btnMobileNav.addEventListener("click", showNavigation);

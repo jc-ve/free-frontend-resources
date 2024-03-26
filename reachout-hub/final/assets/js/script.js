a3ee20990e6113a5326e77f1e5d03cbe34f0104d
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-03-26 22:43:59 +0800

    final proj

--- reachout-hub/final/assets/js/script.js
+++ reachout-hub/final/assets/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var map = L.map("map").setView([51.505, -0.09], 13);
+let map = L.map("map").setView([14.599512, 120.984222], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -8,4 +8,4 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+let marker = L.marker([14.599512, 120.984222]).addTo(map);

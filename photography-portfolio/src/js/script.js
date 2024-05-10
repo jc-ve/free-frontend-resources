01553037c581ee8ad560eb2cc52ad1b8af6edbbb
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-10 23:11:10 +0800

    edit greetings

--- photography-portfolio/src/js/script.js
+++ photography-portfolio/src/js/script.js
@@ -8,23 +8,3 @@ const header = document.querySelector(".header");
 navBtn.addEventListener("click", function () {
   header.classList.toggle("active");
 });
-
-const headingGreeting = document.querySelector(".heading--greeting");
-
-const headerText = [
-  "Hola at me 🤙",
-  "Let's talk ☎️",
-  "Get in touch ✍️",
-  "Hit me up 📲",
-  "Say hello 🙋‍♂️",
-];
-
-let counter = 0;
-
-const changeGreeting = () => {
-  headingGreeting.innerHTML = headerText[counter];
-  counter++;
-  if (counter >= headerText.length) counter = 0;
-};
-
-setInterval(changeGreeting, 3000);

df69435f9490f9b06db35138564fcfc97aedbcc6
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-22 16:02:46 +0800

    added display functionalities & modified structure

--- photography-portfolio/src/js/script.js
+++ photography-portfolio/src/js/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
-// Selectors
-const navBtn = document.querySelector(".btn--navigation");
 const header = document.querySelector(".header");
+const navigation = document.querySelector(".navigation");
+const navigationTabs = document.querySelectorAll(".navigation__tab");
+const navBtn = document.querySelector(".btn--navigation");
+const content = document.querySelectorAll(".content");
 
 navBtn.addEventListener("click", function () {
   header.classList.toggle("active");
+});
+
+navigation.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".navigation__tab");
+
+  if (!clicked) return;
+
+  navigationTabs.forEach((tab) =>
+    tab.classList.remove("navigation__tab--active")
+  );
+  content.forEach((c) => c.classList.remove("content--active"));
+
+  clicked.classList.add("navigation__tab--active");
+
+  document
+    .querySelector(`.content--${clicked.dataset.tab}`)
+    .classList.add("content--active");
 });

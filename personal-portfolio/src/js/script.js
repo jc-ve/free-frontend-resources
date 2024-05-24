ceaa3ee04c2bc6be039f27651fc9b3495ac44c6a
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-24 22:35:31 +0800

    practice strava api

--- personal-portfolio/src/js/script.js
+++ personal-portfolio/src/js/script.js
@@ -54,3 +54,23 @@ const getYoutubeVideos = () => {
 };
 
 getYoutubeVideos();
+
+var StravaApiV3 = require("strava_api_v3");
+var defaultClient = StravaApiV3.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+var strava_oauth = defaultClient.authentications["strava_oauth"];
+strava_oauth.accessToken = "YOUR ACCESS TOKEN";
+
+var api = new StravaApiV3.UploadsApi();
+
+var uploadId = 789; // {Long} The identifier of the upload.
+
+var callback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("API called successfully. Returned data: " + data);
+  }
+};
+api.getUploadById(uploadId, callback);

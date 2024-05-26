ba1a028c806eae7868e262867102dafc715bf9cd
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-26 21:16:23 +0800

    added animation

--- photography-portfolio/src/js/script.js
+++ photography-portfolio/src/js/script.js
@@ -26,3 +26,47 @@ navigation.addEventListener("click", function (e) {
     .querySelector(`.content--${clicked.dataset.tab}`)
     .classList.add("content--active");
 });
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// let sectionsHome = gsap.utils.toArray(".panel");
+
+// gsap.to(sectionsHome, {
+//   xPercent: -100 * (sectionsHome.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".home-container",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sectionsHome.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: "+=3500",
+//   },
+// });
+
+const cards = gsap.utils.toArray(".videos__content");
+
+cards.forEach((card, index) => {
+  const tween = gsap.to(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: () => `top bottom-=100`,
+      end: () => `top top+=40`,
+      scrub: true,
+      invalidateOnRefresh: true,
+    },
+    ease: "none",
+    scale: () => 1 - (cards.length - index) * 0.025,
+  });
+
+  ScrollTrigger.create({
+    trigger: card,
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+
+    id: "pin",
+    end: "max",
+    invalidateOnRefresh: true,
+  });
+});

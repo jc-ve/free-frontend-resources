15b76939959011e20b69ce34fb638834bd9be476
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-28 22:32:45 +0800

    gsap highlight text

--- photography-portfolio/src/js/script.js
+++ photography-portfolio/src/js/script.js
@@ -2,7 +2,7 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".section");
+const sections = gsap.utils.toArray(".section");
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
@@ -14,4 +14,14 @@ gsap.to(sections, {
     snap: 1 / (sections.length - 1),
     end: "+=4000",
   },
+});
+
+const highlightedTexts = gsap.utils.toArray(".highlight");
+
+highlightedTexts.forEach((highlight) => {
+  ScrollTrigger.create({
+    trigger: ".highlight",
+    start: "-100px center",
+    onEnter: () => highlight.classList.add("active"),
+  });
 });

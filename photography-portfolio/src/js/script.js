e657ac435eeda9c075cfe2e3aa27c25a793f8f00
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-30 16:19:40 +0800

    added videography

--- photography-portfolio/src/js/script.js
+++ photography-portfolio/src/js/script.js
@@ -2,6 +2,7 @@
 
 const panels = document.querySelector(".panels");
 const contact = document.querySelector(".contacts");
+const videos = document.querySelector(".videos");
 
 if (panels) {
   gsap.registerPlugin(ScrollTrigger);
@@ -21,6 +22,30 @@ if (panels) {
   });
 }
 
-if (contact) {
-  const textOpts = {};
+if (videos) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+
+  const buildSwiperSlider = (sliderElm) => {
+    console.log(sliderElm.dataset.id);
+    const sliderIdentifier = sliderElm.dataset.id;
+
+    return new Swiper(`.${sliderElm.id}`, {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+    });
+  };
+
+  // Get all of the swipers on the page
+  const allSliders = document.querySelectorAll(".swiper");
+
+  allSliders.forEach((slider) => buildSwiperSlider(slider));
 }
